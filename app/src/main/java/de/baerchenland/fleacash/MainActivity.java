e6ddfe836433003abde8c06ee71ee703efb84f4b
sycorix <sycorix@gmail.com>
2014-10-27 21:05:45 +0100

    fix time stamp 24 hour

--- app/src/main/java/de/baerchenland/fleacash/MainActivity.java
+++ app/src/main/java/de/baerchenland/fleacash/MainActivity.java
@@ -96,7 +96,7 @@ public class MainActivity extends Activity {
 
     public String getCurrentTimeStamp() {
         Date date = new Date();
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd hh:mm:ss");
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
         return (sdf.format(date));
     }
 
